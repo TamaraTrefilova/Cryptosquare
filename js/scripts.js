c9909c201b06b9d858da1ab1ca9f821a6f745e2d
saswati patra and Tamara Trefilova <tamara@>
2019-06-13 15:18:29 -0700

    last commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,7 @@ var row;
 var column;
 var result = "";
 var creatArray = function(string) {
-  string = string.split(" ");
-  string = string.join('');
+  string = string.replace(/[^a-zA-Z0-9]/g, '');
   string = string.split("");
   var stringLength = string.length;
 
@@ -33,52 +32,52 @@ var creatArray = function(string) {
 
 
 var convertArray = function(array) {
-    var stringResult;
-    var countChar = 0;
-    for (var indxCol = 0; indxCol < column; indxCol++) {
-      for (var indxRow = 0; indxRow < row; indxRow++) {
-        if (countChar < 5 && indxRow === row) {
-          indxRow = 0;
-          indxCol = indxCol + 1;
-        } else if (countChar === 5) {
-          result += " ";
-          countChar = 0;
-          indxRow = indxRow - 1;
-        } else {
-          countChar++;
-          var currentChar = array[indxRow][indxCol];
-          if (typeof currentChar =='undefined') {
-              currentChar ="";
-              countChar=countChar-1;
-            }
-            result += currentChar;
-          }
+  var stringResult;
+  var countChar = 0;
+  for (var indxCol = 0; indxCol < column; indxCol++) {
+    for (var indxRow = 0; indxRow < row; indxRow++) {
+      if (countChar < 5 && indxRow === row) {
+        indxRow = 0;
+        indxCol = indxCol + 1;
+      } else if (countChar === 5) {
+        result += " ";
+        countChar = 0;
+        indxRow = indxRow - 1;
+      } else {
+        countChar++;
+        var currentChar = array[indxRow][indxCol];
+        if (typeof currentChar == 'undefined') {
+          currentChar = "";
+          countChar = countChar - 1;
         }
-
+        result += currentChar;
       }
-      return result;
     }
 
+  }
+  return result;
+}
 
 
 
 
 
 
-    $(function() {
-      $("form").submit(function(event) {
-        event.preventDefault();
-        // debugger;
-        var result1;
-        var initString = $("input#numberInput").val();
-        var firstArray = creatArray(initString);
-        result1 = convertArray(firstArray);
-        alert(result1);
 
-        $("#outputA").text(initString);
-        $("#outputB").text(result1);
-        $("#output").show();
+$(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    // debugger;
+    var result1;
+    var initString = $("input#numberInput").val();
+    var firstArray = creatArray(initString);
+    result1 = convertArray(firstArray);
+    alert(result1);
 
-      });
+    $("#outputA").text(initString);
+    $("#outputB").text(result1);
+    $("#output").show();
 
-    });
+  });
+
+});
